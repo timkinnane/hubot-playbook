b87115c16270c3cee6960023d90df9a28c65a15e
timkinnane <tim@nestedcode.com>
2017-05-19 15:23:34 +1000

    chore(demo): Name correction

--- demo/bin/hubot.cmd
+++ demo/bin/hubot.cmd
@@ -4,4 +4,4 @@ call npm install
 SETLOCAL
 SET PATH=node_modules\.bin;node_modules\hubot\node_modules\.bin;%PATH%
 
-node_modules\.bin\hubot.cmd --name "playbook-dmeo" %* 
+node_modules\.bin\hubot.cmd --name "playbook-demo" %*
